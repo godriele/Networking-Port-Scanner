4a5788b45a6c4362ec35d8de2b815252be991cfa
Godriele Dela Cruz <godriele11@yahoo.com>
2025-01-07 11:05:56 -0800

    Worker function to get the queue

--- app/app.py
+++ app/app.py
@@ -30,9 +30,31 @@ def portscan(port):
 
 # print(portscan(80))
 
-for port in range(1, 1024):
-    result = portscan(port)
-    if(result):
-        print("Port {} is open!".format(port))
-    else:
-        print("Port {} is closed!".format(port))
+# For loop to check for ports
+# for port in range(1, 1024):
+#     result = portscan(port)
+#     if(result):
+#         print("Port {} is open!".format(port))
+#     else:
+#         print("Port {} is closed!".format(port))
+        
+
+from queue import Queue # Queueing the port numbers, everytime I get a port, it will shift to a new one
+import threading 
+
+queue = Queue()
+open_ports = []
+
+def fill_queue(port_list):
+    for port in port_list:
+        queue.put(port)
+        
+def worker():
+    while not queue.empty():
+        port = queue.get()
+        if portscan(port):
+            print("Port {} is open!".format(port))
+            open_ports.append(port)
+        else:
+            print("Port {} is closed!".format(port))
+    
