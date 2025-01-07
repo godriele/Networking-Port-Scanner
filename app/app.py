ce5693c4b9970137b789645f3511ad88635e6ef9
Godriele Dela Cruz <godriele11@yahoo.com>
2025-01-07 10:29:42 -0800

    Explained the script/Test push

---
+++ app/app.py
@@ -0,0 +1,17 @@
+# This will be just for educational purposes
+# Credits to NeuraNine
+
+'''
+Port Scanners are primarily used for Penetration Testing and Information Gathering
+I will be just practicing how to scan ports using my own network 
+
+Basic Functionality
+ - A port scanner tries to connect to an IP-Address on a certain port. Usually when we
+ surf the web, we connect to servers via port 80 (HTTP) or port 43 (HTTPS).
+  But there are also a lot of other crucial ports like 21 (FTP), 22 (SSH), 25 (SMTP) 
+  and many more. In fact, there are more than 130,000 ports of which 1,023 are standardized 
+  and 48,128 reserved. So, when we build a port scanner, we better make it efficient and focus
+  on the crucial ports.
+'''
+
+# Simplest Way
